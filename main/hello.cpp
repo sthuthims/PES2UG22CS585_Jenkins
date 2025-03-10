e74d42e4997102c5f1001de8c778cb5fcf7e1cae
sthuthims <sthuthimjalu@gmail.com>
2025-03-10 16:27:32 +0530

    changes made

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
+  cout << "Hello,585_LAB8\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   return 0;
